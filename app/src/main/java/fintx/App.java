a8758e91fc0e5cf6cb5a5dec80c1a45a9d175c0e
Mark DeSpain <mark.despain@gmail.com>
2023-02-07 20:08:09 +0900

    accept a file as a command line parameter

--- app/src/main/java/fintx/App.java
+++ app/src/main/java/fintx/App.java
@@ -3,19 +3,24 @@
  */
 package fintx;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 
 @CommandLine.Command
 public class App implements Callable<Integer> {
 
+    @CommandLine.Parameters(description = "file to digest")
+    private File file;
+
     public static void main(String[] args) {
-        new CommandLine(new App()).execute(args);
+        final int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 
     @Override
     public Integer call() {
-        System.out.println("Hello World!");
+        System.out.println("received file: " + file.getAbsolutePath());
         return 0;
     }
 }
