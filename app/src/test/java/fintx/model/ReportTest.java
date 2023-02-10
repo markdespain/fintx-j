152952662946660db183c91c63072331df4028d8
Mark DeSpain <mark.despain@gmail.com>
2023-02-10 11:24:25 +0900

    fix: add test body to ReportTest

--- app/src/test/java/fintx/model/ReportTest.java
+++ app/src/test/java/fintx/model/ReportTest.java
@@ -4,11 +4,14 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.Collections;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class ReportTest {
+    private static final boolean NO_ERRORS = false;
+    private static final boolean HAS_ERRORS = true;
 
     public static Stream<Arguments> testHasErrors() {
 
@@ -29,32 +32,34 @@ public class ReportTest {
 
         return Stream.of(
                 arguments(
+                        NO_ERRORS,
                         ImmutableReport.builder()
                                 .rakutenFileInfo(noErrors)
                                 .genericFileInfo(noErrors)
-                                .build(),
-                        false),
+                                .build()),
                 arguments(
+                        HAS_ERRORS,
                         ImmutableReport.builder()
                                 .rakutenFileInfo(hasErrors)
                                 .genericFileInfo(noErrors)
-                                .build(),
-                        true),
+                                .build()),
                 arguments(
+                        HAS_ERRORS,
                         ImmutableReport.builder()
                                 .rakutenFileInfo(noErrors)
                                 .genericFileInfo(hasErrors)
-                                .build(),
-                        true),
+                                .build()),
                 arguments(
+                        HAS_ERRORS,
                         ImmutableReport.builder()
                                 .rakutenFileInfo(hasErrors)
                                 .genericFileInfo(hasErrors)
-                                .build(),
-                        true));
+                                .build()));
     }
 
     @ParameterizedTest
     @MethodSource
-    void testHasErrors(final Report report, final boolean expected) {}
+    void testHasErrors(final boolean expected, final Report report) {
+        Assertions.assertEquals(expected, report.hasErrors());
+    }
 }
